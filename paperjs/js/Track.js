f2e00dd1817cea241bea2494480582c9283b350b
Oliviya11 <nastyawnastya@gmail.com>
2018-02-16 11:29:27 +0200

    Move all files to paperjs folder

--- paperjs/js/Track.js
+++ paperjs/js/Track.js
@@ -7,11 +7,11 @@ var WINDOW_HEIGHT = global.window_height;
 function createTrack () {
   var trackColor = '#F0E68C';
   track = new Path();
-  track.add(new Point(WINDOW_WIDTH*0.125, WINDOW_HEIGHT*3.2));
-  track.add(new Point(WINDOW_WIDTH*0.125, WINDOW_HEIGHT*0.6));
+  track.add(new Point(-WINDOW_WIDTH*0.2, WINDOW_HEIGHT*6));
+  track.add(new Point(WINDOW_WIDTH*0.125, WINDOW_HEIGHT*0.8));
   track.add(new Point(WINDOW_WIDTH*0.9, WINDOW_HEIGHT*0.2));
-  track.add(new Point(WINDOW_WIDTH*0.5, WINDOW_HEIGHT*0.8));
-  track.add(new Point(WINDOW_WIDTH*0.27, WINDOW_HEIGHT*0.76));
+  track.add(new Point(WINDOW_WIDTH*0.5, WINDOW_HEIGHT*0.9));
+  track.add(new Point(WINDOW_WIDTH*0.27, WINDOW_HEIGHT*0.8));
   track.add(new Point(WINDOW_WIDTH*0.32, WINDOW_HEIGHT*0.58));
   track.add(new Point(WINDOW_WIDTH*0.48, WINDOW_HEIGHT*0.44));
   track.add(new Point(WINDOW_WIDTH*0.7, WINDOW_HEIGHT*0.35));
